7591aa2986ba66e1ae521af4a0eefe170ccff569
netanel segal <segal.netanel4@gmail.com>
2026-04-26 11:20:02 +0300

    up

--- src/sections/landing/LeadCaptureSection.tsx
+++ src/sections/landing/LeadCaptureSection.tsx
@@ -19,7 +19,7 @@ import {
   toLandingSubmission,
 } from '@/schemas/contact';
 import Recaptcha from 'react-google-recaptcha';
-import { validatedEnv } from '@/lib/env';
+import { publicEnv } from '@/lib/env-public';
 import { useRecaptchaField } from '@/hooks/useRecaptchaField';
 
 const defaultValues: LeadCaptureFormInput = {
@@ -196,7 +196,7 @@ const LeadCaptureSection = ({
           <div className='md:col-span-2 flex flex-col items-center gap-2'>
             <Recaptcha
               ref={recaptchaRef}
-              sitekey={validatedEnv.recaptchaSiteKey}
+              sitekey={publicEnv.recaptchaSiteKey}
               onChange={onTokenChange}
               onExpired={() => onTokenChange(null)}
             />
