6893d703c00f434bef237e0d52e577ece041ac64
LeadAIO <it@leadaio.com>
2026-04-30 17:09:03 -0700

    Update layout.tsx
    - Added Google Tag Manager and Google Search Console verification scripts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,9 @@ const fontBebasNeue = Bebas_Neue({
 
 export const metadata: Metadata = {
   metadataBase: new URL(validatedEnv.siteUrl),
+  verification: {
+    google: 'nF98nC7bk49lYVpfTPwW2iwQoG946vEGm9wVl3lzYU4',
+  },
   ...generateSocialMetadata({
     title: 'Fast Struct | Modular & Panelized Homes CA',
     description:
@@ -38,10 +41,30 @@ export default async function RootLayout({
   return (
     <html lang='en'>
       <body className={`${fontPoppins.variable} ${fontBebasNeue.variable}`}>
+        <Script id='google-tag-manager' strategy='afterInteractive'>
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-P458M4JF');
+          `}
+        </Script>
+
+        <noscript>
+          <iframe
+            src='https://www.googletagmanager.com/ns.html?id=GTM-P458M4JF'
+            height='0'
+            width='0'
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+
         <Script
           src='https://kit.fontawesome.com/42dfb7600e.js'
           crossOrigin='anonymous'
         />
+
         <ReactLenis
           root
           options={{
